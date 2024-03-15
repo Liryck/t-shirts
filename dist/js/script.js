45357fc2ff3dea600ea5684d32eff15fbb0b51f5
Liryck <Liryckbragin@gmail.com>
2024-03-15 22:47:30 +0200

    add galery to pages with tshirts, but have a problem with slider

--- dist/js/script.js
+++ dist/js/script.js
@@ -96,41 +96,7 @@ $(document).ready(function(){
     }
 
     slidesField.style.transform = `translateX(-${offset}px)`;
-    }    
-
-    // Color changing
-
-    
-
-    // Simple slider:
-
-    // showSlides(slideIndex);
-
-    // function showSlides(n) {
-    //     if (n > slides.length) {
-    //         slideIndex = 1;
-    //     }
-
-    //     if (n < 1) {
-    //         slideIndex = slides.length;
-    //     }
-
-    //     slides.forEach(item => item.style.display = 'none');
-
-    //     slides[slideIndex - 1].style.display = 'block';
-    // }
-
-    // function plusSlides(n) {
-    //     showSlides(slideIndex += n);
-    // }
-
-    // prev.addEventListener('click', () => {
-    //     plusSlides(-1);
-    // });
-
-    // next.addEventListener('click', () => {
-    //     plusSlides(1);
-    // });
+    }
 
 
     // Form Validate
